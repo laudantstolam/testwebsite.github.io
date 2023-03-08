4c8171f65f42f82703ad05e1d81595fa87de554e
laudantstolam <rachelle0607@gmail.com>
2023-03-08 16:16:33 +0800

    adding first target and adjust icon mg position

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
 import './App.css';
 import HelmetExport from 'react-helmet';
 //image
-import tw from "./twittercon.png";// silly bird
-import yt from "./button_subscribe.png";
+// import tw from "./twittercon.png";// silly bird
+// import yt from "./button_subscribe.png";
 
 //firebase related
 // import startfirebase from './config';
 // //import {ref,set,get,update,remove,child} from 'firebase/database';
 // import 'firebase/database';
 import MyComponent from './firebaseconfig';
-import React from 'react';
+import React, { useState } from 'react';
 //import firebase from 'firebase/compat/app';
 import 'firebase/database';
-
+import house_1 from './target';
 function App() {
   const twlink = "https://twitter.com/shu_yamino";
   const y2link = "https://www.youtube.com/@ShuYamino";
   return (
     <div className="App">
       <div>
-      <HelmetExport>
-      <link rel="icon" href="%PUBLIC_URL%/yaminerd.png" />
-      {/* 使用https://favicon.io/favicon-converter/ 轉換 路徑必須放在public 下 */}
-      <title>What-does-shu-say</title>
-      </HelmetExport>
+        <HelmetExport>
+          <link rel="icon" href="%PUBLIC_URL%/yaminerd.png" />
+          {/* 使用https://favicon.io/favicon-converter/ 轉換 路徑必須放在public 下 */}
+          <title>What-does-shu-say</title>
+        </HelmetExport>
       </div>
-      
+
       <div className='content'>
-        <p>123,456,789</p>
+        <button>
+          <img src="./image/t1.png" width={180} id="target_1" alt='stage1_1' />
+        </button>
+        <house_1 />
       </div>
       {/* <MyComponent /> */}
-     
-      
-        <footer>
-          <a href={twlink}><img src={tw} alt="icon" width={90}></img></a>
-          <a href={y2link}><img src={yt} alt="icon" width={90}></img></a>
-        </footer>
+
+
+      <footer>
+        <a href={twlink}><img src='./image/twittercon.png' alt="icon" width={90}></img></a>
+        <a href={y2link}><img src='./image/button_subscribe.png' alt="icon" width={90}></img></a>
+      </footer>
     </div>
   );
 }
