8a2e9ebad950630a1b53ab133be2cbc83ba5e2f7
laudantstolam <rachelle0607@gmail.com>
2023-04-03 01:17:47 +0800

    adding all voice and some warning adjustment

--- src/alert.js
+++ src/alert.js
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
-import { Alert, Button } from 'react-bootstrap';
-import './App.css';
+import React, { useState } from "react";
+import { Alert, Button } from "react-bootstrap";
+import "./App.css";
 
 function AlertMessage() {
-    const [showAlert, setShowAlert] = useState(true);
+  const [showAlert, setShowAlert] = useState(true);
 
-    const handleAlertClose = () => setShowAlert(false);
+  const handleAlertClose = () => setShowAlert(false);
 
-    return (
-        <>
-            {showAlert && (
-                <Alert variant="info" onClose={handleAlertClose} dismissible id='alertpage'>
-                    Welcome this is a test alert<br />
-                    <Button variant="outline-info" size="sm" onClick={handleAlertClose} className="ml-2">
-                        <img src="./image/buttin_off.png" width={40} alt='alertoff' />
-                    </Button>
-                </Alert>
-            )}
-        </>
-    );
+  return (
+    <>
+      {showAlert && (
+        <Alert
+          variant="info"
+          onClose={handleAlertClose}
+          dismissible
+          id="alertpage"
+        >
+          Welcome eyyyyy
+          <br />
+          this site is powered by birthdayShu2023 team
+          <br />
+          try to use UNKO-KUN(the corsur)
+          <br />
+          to click on anything you can
+          <br />
+          <Button
+            variant="outline-info"
+            size="lg"
+            onClick={handleAlertClose}
+            className="ml-2"
+          >
+            <br />{" "}
+            <img src="./image/buttin_off.png" width={40} alt="alertoff" />
+          </Button>
+        </Alert>
+      )}
+    </>
+  );
 }
 
 export default AlertMessage;
